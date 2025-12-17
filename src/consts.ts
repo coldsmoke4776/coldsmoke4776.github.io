f2467207c3728fb0f8a53b047ba5db7af600c5df
Matthew Twells <matttwells@outlook.com>
2025-12-16 20:07:15 -0800

    updated eagles record

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 export const SITE_TITLE = "Matt Twells (jawndeere)";
 export const SITE_DESCRIPTION = "A personal portfolio showcasing projects, blog posts, and programming insights.";
 export const GITHUB_USERNAME = "coldsmoke4776"; // Replace with your actual GitHub username
-export const QUOTE = "Solutions Engineering Lead • Systems & Security Explorer • Amateur Game Dev • GO BIRDS (8-4)";
+export const QUOTE = "Solutions Engineering Lead • Systems & Security Explorer • Amateur Game Dev • GO BIRDS (9-5)";
 
 export const KNOWN_TECH = [
   "Astro",
